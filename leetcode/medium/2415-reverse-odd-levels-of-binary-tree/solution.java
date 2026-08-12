ca6cf27ee98992bf0bdff2b9aa580e28af16102e
Aditya Devan <iamadyadevan@gmail.com>
2026-08-12 22:12:34 +0530

    ✅ Reverse Odd Levels of Binary Tree | Medium | Java | Runtime: 10 ms (39.43%) | Memory: 51.2 MB | 2026-08-12

---
+++ leetcode/medium/2415-reverse-odd-levels-of-binary-tree/solution.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public TreeNode reverseOddLevels(TreeNode root) {
+        if (root == null) return root;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int level = 0;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<TreeNode> currentLevel = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                currentLevel.add(node);
+
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+
+            if (level % 2 == 1) {
+                int left = 0, right = currentLevel.size() - 1;
+                while (left < right) {
+                    int temp = currentLevel.get(left).val;
+                    currentLevel.get(left).val = currentLevel.get(right).val;
+                    currentLevel.get(right).val = temp;
+                    left++;
+                    right--;
+                }
+            }
+            level++;
+        }
+
+        return root;
+    }
+}
